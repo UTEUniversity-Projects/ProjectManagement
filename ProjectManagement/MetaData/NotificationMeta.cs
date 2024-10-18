a177321fc486d3fa1116698408de6ad30775e1a6
Le Hong Phuc <84840881+PhucLe0809@users.noreply.github.com>
2024-10-18 21:35:48 +0700

    Trial version lhphuc (#11)
    * add resource files
    
    * delete old virtual data
    
    * add member role in team
    
    * modify approve team for project
    
    * add notification feature

---
+++ ProjectManagement/MetaData/NotificationMeta.cs
@@ -0,0 +1,32 @@
+﻿using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.MetaData
+{
+    public class NotificationMeta
+    {
+        private Notification notification;
+        private bool isSaw;
+
+        public NotificationMeta()
+        {
+            this.notification = new Notification();
+            this.isSaw = false;
+        }
+        public NotificationMeta(Notification notification, bool isSaw)
+        {
+            this.notification = notification;
+            this.isSaw = isSaw;
+        }
+
+        public Notification Notification { get { return this.notification; } }
+        public bool IsSaw { 
+            get { return this.isSaw; }
+            set { this.isSaw = value; }
+        }
+    }
+}
