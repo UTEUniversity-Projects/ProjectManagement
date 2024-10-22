db576921b7becaa174304170c2f40fcb5dccc9ba
hnhung31 <hnhunght31.10@gmail.com>
2024-10-23 00:10:28 +0700

    nhung modifi: MettingDao, CommentDao, MeetingModel, CommentModel

--- ProjectManagement/DAOs/MeetingDAO.cs
+++ ProjectManagement/DAOs/MeetingDAO.cs
@@ -18,11 +18,46 @@ namespace ProjectManagement.DAOs
 
         public static Meeting SelectOnly(string meetingId)
         {
-            return DBGetModel.GetModel(DBTableNames.Meeting, "meetingId", meetingId, new MeetingMapper());
+            string sqlStr = $"SELECT * FROM FUNC_GetMeetingById(@meetingId)";
+
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@meetingId", meetingId)
+            };
+
+            DataTable dataTable = DBExecution.SQLExecuteQuery(sqlStr, parameters, string.Empty);
+
+            if (dataTable != null && dataTable.Rows.Count > 0)
+            {
+                MeetingMapper meetingMapper = new MeetingMapper();
+                return meetingMapper.MapRow(dataTable.Rows[0]);
+            }
+
+            return null;
         }
         public static List<Meeting> SelectByProject(string projectId)
         {
-            return DBGetModel.GetModelList(DBTableNames.Meeting, "projectId", projectId, new MeetingMapper());
+            string sqlStr = string.Format("SELECT * FROM FUNC_GetMeetingsByProjectId(@projectId)");
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@projectId", projectId)
+            };
+
+            DataTable dataTable = DBExecution.SQLExecuteQuery(sqlStr, parameters, string.Empty);
+
+            List<Meeting> listMeetings = new List<Meeting>();
+
+            if (dataTable != null && dataTable.Rows.Count > 0)
+            {
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    Meeting meeting = DBGetModel.GetModelFromDataRow(row, new MeetingMapper());
+                    listMeetings.Add(meeting);
+                }
+            }
+
+            return listMeetings;
+
         }
 
         #endregion
@@ -31,18 +66,93 @@ namespace ProjectManagement.DAOs
 
         public static void Insert(Meeting meeting)
         {
-            DBExecution.Insert(meeting, DBTableNames.Meeting);
+            string sqlStr = "EXEC CreateMeeting @meetingId, @title, @description, @startAt, @location, @link, @createdAt, @createdBy, @projectId";
+             
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                    new SqlParameter("@meetingId", meeting.MeetingId),
+                    new SqlParameter("@title", meeting.Title),
+                    new SqlParameter("@description", meeting.Description),
+                    new SqlParameter("@startAt", meeting.StartAt),
+                    new SqlParameter("@location", meeting.Location),
+                    new SqlParameter("@link", meeting.Link),
+                    new SqlParameter("@createdAt", meeting.CreatedAt),
+                    new SqlParameter("@createdBy", meeting.CreatedBy),
+                    new SqlParameter("@projectId", meeting.ProjectId)
+            };
+            DBExecution.SQLExecuteNonQuery(sqlStr, parameters, "Create Meeting"); ;
         }
         public static void Delete(Meeting meeting)
         {
-            DBExecution.Delete(DBTableNames.Meeting, "meetingId", meeting.MeetingId);
+            string procedureName = "dbo.PROC_DeleteMeeting";
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@MeetingId", meeting.MeetingId)
+            };
+            DBExecution.ExecuteStoredProcedure(procedureName, parameters, "Delete Meeting");
+
         }
         public static void Update(Meeting meeting)
         {
-            DBExecution.Update(meeting, DBTableNames.Meeting, "meetingId", meeting.MeetingId);
+            string procedureName = "dbo.PROC_UpdateMeeting";
+
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@MeetingId", meeting.MeetingId),
+                new SqlParameter("@Title", meeting.Title),
+                new SqlParameter("@Description", meeting.Description),
+                new SqlParameter("@StartAt", meeting.StartAt),
+                new SqlParameter("@Location", meeting.Location),
+                new SqlParameter("@Link", meeting.Link),
+                new SqlParameter("@CreatedAt", meeting.CreatedAt),
+                new SqlParameter("@CreatedBy", meeting.CreatedBy),
+                new SqlParameter("@ProjectId", meeting.ProjectId)
+            };
+
+            DBExecution.ExecuteStoredProcedure(procedureName, parameters, string.Empty);
         }
 
         #endregion
 
+        #region CHECK INFORMATIONS
+
+        public static bool CheckIsNotEmpty(string input, string fieldName)
+        {
+            string sqlStr = "SELECT IsValid FROM dbo.IsNotEmpty(@input, @fieldName)";
+
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@input", input),
+                new SqlParameter("@fieldName", fieldName)
+            };
+
+            DataTable dataTable = DBExecution.SQLExecuteQuery(sqlStr, parameters, string.Empty);
+
+            if (dataTable != null && dataTable.Rows.Count > 0)
+            {
+                return Convert.ToBoolean(dataTable.Rows[0]["IsValid"]);
+            }
+
+            return false;
+        }
+        public static bool CheckStart(DateTime startAt, string fieldName)
+        {
+            string sqlStr = "SELECT IsValid FROM dbo.FUNC_CheckStartDate(@startAt, @fieldName)";
+
+            List<SqlParameter> parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@startAt", startAt),
+                new SqlParameter("@fieldName", fieldName)
+            };
+
+            DataTable dataTable = DBExecution.SQLExecuteQuery(sqlStr, parameters, string.Empty);
+
+            if (dataTable != null && dataTable.Rows.Count > 0)
+            {
+                return Convert.ToBoolean(dataTable.Rows[0]["IsValid"]);
+            }
+            return false;
+        }
+        #endregion
     }
 }
